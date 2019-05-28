496d3b8e894ea1ab8e4e353fb95f19e292270601
jwlodek <jwlodek@cs.stonybrook.edu>
2019-05-28 11:30:43 -0400

    adding a cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#
+# Script that cleans up IOC directory after ioc-template has been cloned
+#
+# Author: Jakub Wlodek
+#
+
+rm -rf dependancyScripts/
+rm -rf startupScripts/
+rm -rf logs/
+rm README.md
+mkdir autosave
+rm cleanup.sh
