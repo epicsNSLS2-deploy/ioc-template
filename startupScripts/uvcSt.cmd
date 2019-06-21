d4adafc6993ee82e418abe93a5c539758f712ba0
jwlodek <jwlodek@cs.stonybrook.edu>
2019-06-21 15:54:57 -0400

    Added simm detector IOC to the ioc-template

--- startupScripts/uvcSt.cmd
+++ startupScripts/uvcSt.cmd
@@ -3,7 +3,7 @@
 < unique.cmd
 errlogInit(20000)
 
-< /epics/prod/Deb7/R3-4/envPaths
+< envPaths
 
 #epicsThreadSleep(20)
 dbLoadDatabase("$(ADUVC)/iocs/adUVCIOC/dbd/adUVCApp.dbd")
@@ -37,32 +37,28 @@ epicsThreadSleep(2)
 asynSetTraceIOMask($(PORT), 0, 2)
 #asynSetTraceMask($(PORT),0,0xff)
 
+# Load the templates for camera PVs
 dbLoadRecords("$(ADCORE)/db/ADBase.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(ADUVC)/db/ADUVC.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+
+
 #
 # Create a standard arrays plugin, set it to get data from Driver.
 #int NDStdArraysConfigure(const char *portName, int queueSize, int blockingCallbacks, const char *NDArrayPort, int NDArrayAddr, int maxBuffers, size_t maxMemory,
 #                          int priority, int stackSize, int maxThreads)
 NDStdArraysConfigure("Image1", 3, 0, "$(PORT)", 0)
-#dbLoadRecords("$(ADCORE)/db/NDPluginBase.template","P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
-#dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int16,SIZE=16,FTVL=SHORT,NELEMENTS=802896")
 dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,NDARRAY_PORT=$(PORT),TIMEOUT=1,TYPE=Int16,FTVL=SHORT,NELEMENTS=20000000")
-#
+
+
 # Load all other plugins using commonPlugins.cmd
 < $(ADCORE)/iocBoot/commonPlugins.cmd
-#
-#Note mpi control pipe out & in reversed.  Names are from the view of the MPI program.
-#NDPipeWriterConfigure("PipeWriter1", 15000, 0, "$(PORT)", "/local/xpcscmdout", "/local/xpcscmdin", 0, 0, 0, 0,0)
-#dbLoadRecords("$(ADCORE)/db/NDPluginPipeWriter.template", "P=$(PREFIX),R=PW1:,  PORT=PipeWriter1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),CMD_IN_PORT=PW_CMD_IN,CMD_OUT_PORT=PW_CMD_OUT")
-
-#Note Local plugin to run the IMM plugin writer
-#NDFileIMMConfigure("IMM1", 15000, 0, "$(PORT)",  0, 0, 0)
-#dbLoadRecords("$(ADCORE)/db/NDFileIMM.template", "P=$(PREFIX),R=IMM1:,PORT=IMM1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT)")
 
 set_requestfile_path("$(ADUVC)/adUVCApp/Db")
 
+# debug sets
 #asynSetTraceMask($(PORT),0,0x09)
 #asynSetTraceMask($(PORT),0,0x11)
+
 iocInit()
 
 # save things every thirty seconds
